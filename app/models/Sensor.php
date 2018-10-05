b1fb2725c55f004b48066d0868cb9a3d66d58b99
Benkessl <38516389+Benkessl@users.noreply.github.com>
2018-10-05 11:06:18 -0500

    Added sensor PHP files

---
+++ app/models/Sensor.php
@@ -0,0 +1,35 @@
+<?php
+
+class Sensor
+{
+  public $sensorId;
+  public $sensorName;
+  public $sensorDescription;
+  public $manufacturer;
+  public $totalLifeExpectancyHours;
+
+  public function __construct($row) {
+    $this->sensorId = isset($row['sensorId']) ? intval($row['sensorId']) : null;
+    $this->sensorName = $row['sensorName'];
+    $this->sensorDescription = $row['sensorDescription'];
+    $this->manufacturer = $row['manufacturer'];
+    $this->totalLifeExpectancyHours = intval($row['totalLifeExpectancyHours']);
+  }
+
+  public static function findAll() {
+    // 1. Connect to the database
+    $db = new PDO(DB_SERVER, DB_USER, DB_PW);
+    // 2. Prepare the query
+    $sql = 'SELECT * FROM Sensor';
+    $statement = $db->prepare($sql);
+    // 3. Run the query
+    $success = $statement->execute();
+    // 4. Handle the results
+    $arr = [];
+    while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
+      $sensorItem =  new Sensor($row);
+      array_push($arr, $sensorItem);
+    }
+    return $arr;
+  }
+}
