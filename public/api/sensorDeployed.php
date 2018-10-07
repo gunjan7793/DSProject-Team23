baef8788450ef2f139c8a5e18f638de7a62dd339
Benkessl <38516389+Benkessl@users.noreply.github.com>
2018-10-07 18:03:05 -0400

    API Fix

--- public/api/sensorDeployed.php
+++ public/api/sensorDeployed.php
@@ -1,35 +1,11 @@
 <?php
 
-class SensorDeployed
-{
-  public $sensorDeployedId;
-  public $sensorId;
-  public $turbineDeployedId;
-  public $serialNumber;
-  public $deployedDate;
+require '../../app/common.php';
 
-  public function __construct($row) {
-    $this->sensorDeployedId = isset($row['sensorDeployedId']) ? intval($row['sensorDeployedId']) : null;
-    $this->sensorId = intval($row['sensorId']);
-    $this->turbineDeployedId = intval($row['turbineDeployedId']);
-    $this->serialNumber = intval($row['serialNumber']);
-    $this->deployedDate = $row['deployedDate'];
-  }
-
-  public static function findAll() {
-    // 1. Connect to the database
-    $db = new PDO(DB_SERVER, DB_USER, DB_PW);
-    // 2. Prepare the query
-    $sql = 'SELECT * FROM SensorDeployed';
-    $statement = $db->prepare($sql);
-    // 3. Run the query
-    $success = $statement->execute();
-    // 4. Handle the results
-    $arr = [];
-    while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
-      $sensorDeployedItem =  new SensorDeployed($row);
-      array_push($arr, $sensorDeployedItem);
-    }
-    return $arr;
-  }
-}
+// 1. Go to the database and get all work associated with the $taskId
+$sensorDeployedAll = SensorDeployed::findAll();
+// 2. Convert to JSON
+$json = json_encode($sensorDeployedAll, JSON_PRETTY_PRINT);
+// 3. Print
+header('Content-Type: application/json');
+echo $json;
