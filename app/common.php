faf669145ed3d132e53d886556f38e7b06a64c9b
zhang455 <zhang455@iu.edu>
2018-10-21 22:30:51 -0400

    change php

--- app/common.php
+++ app/common.php
@@ -1,13 +1,6 @@
 <?php
 
-// Change the working directory to this file.
-chdir(__DIR__);
-set_include_path (__DIR__);
 
-if ($_SERVER['REQUEST_METHOD'] == 'POST'
-&& stripos($_SERVER['CONTENT_TYPE'], 'application/json') !== false ) {
-  $_POST = json_decode(file_get_contents('php://input'), true);
-}
 
 // require 'environment.php';
 
