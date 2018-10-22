144f8f7cb6dd636af87c13608be92dab0bdb779e
zhang455 <zhang455@iu.edu>
2018-10-21 22:45:14 -0400

    testing turbine deployed

--- public/api/turbineDeployed.php
+++ public/api/turbineDeployed.php
@@ -12,8 +12,7 @@ if ($turbineId < 1) {
 
 // 1. Go to the database and get all work associated with the $taskId
 $turbineDeployAll = TurbineDeployed::findTurbineFromSiteTurbineId([$siteId],[$turbineId]);
-// 2. Convert to JSON
-$json = json_encode($turbineDeployAll, JSON_PRETTY_PRINT);
+
 // 3. Print
 header('Content-Type: application/json');
 echo $json;
