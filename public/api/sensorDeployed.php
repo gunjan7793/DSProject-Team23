801f2af8651e232a67c77ad1ba8c442d44d645d9
Benkessl <38516389+Benkessl@users.noreply.github.com>
2018-10-23 02:57:14 -0400

    style

--- public/api/sensorDeployed.php
+++ public/api/sensorDeployed.php
@@ -2,10 +2,10 @@
 
 require '../../app/common.php';
 
-// 1. Go to the database and get all work associated with the $taskId
 $sensorDeployedAll = SensorDeployed::findAll();
-// 2. Convert to JSON
+
 $json = json_encode($sensorDeployedAll, JSON_PRETTY_PRINT);
-// 3. Print
+
 header('Content-Type: application/json');
+
 echo $json;
--- public/api/site.php
+++ public/api/site.php
@@ -6,10 +6,10 @@ if ($clientId < 1) {
   throw new Exception('Invalid Client ID');
 }
 
-// 1. Go to the database and get all work associated with the $taskId
 $siteAll = Site::findSiteFromClientId([$clientId]);
-// 2. Convert to JSON
+
 $json = json_encode($siteAll, JSON_PRETTY_PRINT);
-// 3. Print
+
 header('Content-Type: application/json');
+
 echo $json;
