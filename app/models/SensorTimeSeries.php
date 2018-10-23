890b3a953e3591907fb3d124e3698cb491619407
Benkessl <38516389+Benkessl@users.noreply.github.com>
2018-10-23 03:12:09 -0400

    Sum hours function

--- app/models/SensorTimeSeries.php
+++ app/models/SensorTimeSeries.php
@@ -42,4 +42,20 @@ class SensorTimeSeries
     }
     return $arr;
   }
+  public static function findTotalFiredHours() {
+    // 1. Connect to the database
+    $db = new PDO(DB_SERVER, DB_USER, DB_PW);
+    // 2. Prepare the query
+    $sql = 'SELECT SUM(firedHours) FROM SensorTimeSeries';
+    $statement = $db->prepare($sql);
+    // 3. Run the query
+    $success = $statement->execute();
+    // 4. Handle the results
+    $arr = [];
+    while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
+      $clientItem =  new Client($row);
+      array_push($arr, $clientItem);
+    }
+    return $arr;
+  }
 }
