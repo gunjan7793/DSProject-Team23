7c8ba19a7e39c07bdd5ff7c6341df0269679c9f5
Benkessl <38516389+Benkessl@users.noreply.github.com>
2018-10-23 03:06:53 -0400

    Added function to client

--- app/models/Client.php
+++ app/models/Client.php
@@ -52,4 +52,20 @@ class Client
     }
     return $arr;
   }
+  public static function retrieveClientList() {
+    // 1. Connect to the database
+    $db = new PDO(DB_SERVER, DB_USER, DB_PW);
+    // 2. Prepare the query
+    $sql = 'SELECT clientName FROM Client';
+    $statement = $db->prepare($sql);
+    // 3. Run the query
+    $success = $statement->execute();
+    // 4. Handle the results
+    $arr = [];
+    while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
+      $clientItem =  new Client($row);
+      array_push($arr, $clientItem);
+    }
+    return $arr;
+  }
 }
