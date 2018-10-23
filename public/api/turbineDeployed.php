dd1a72936182f1333fdd52cf4b73502ec14a71c6
Benkessl <38516389+Benkessl@users.noreply.github.com>
2018-10-23 02:54:36 -0400

    Minor Style fixes

--- public/api/turbineDeployed.php
+++ public/api/turbineDeployed.php
@@ -9,11 +9,9 @@ $turbineId = intval($_GET['turbineId'] ?? 0);
 if ($turbineId < 1) {
   throw new Exception('Invalid Turbine ID');
 }
-
-// 1. Go to the database and get all work associated with the $taskId
 $turbineDeployAll = TurbineDeployed::findTurbineFromSiteTurbineId([$siteId],[$turbineId]);
-// 2. Convert to JSON
+
 $json = json_encode($turbineDeployAll, JSON_PRETTY_PRINT);
-// 3. Print
+
 header('Content-Type: application/json');
 echo $json;
