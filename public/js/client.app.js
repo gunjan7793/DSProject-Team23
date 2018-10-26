880c27427d76e24b31af4bf9cfbeee5a5bff4f3c
gunjan7793 <42595749+gunjan7793@users.noreply.github.com>
2018-10-25 22:14:54 -0400

    modal for notes

--- public/js/client.app.js
+++ public/js/client.app.js
@@ -7,35 +7,72 @@ var clientApp = new Vue({
 				"notes":'',
 				"clientId":''
 			}
-		]
+		],
+		noteForm:{},
+		id:	''
 	},
 	computed: {},
 	methods: {
 		goToSites(clientId){
 			window.location='sites.html?clientId='+clientId;
 		},
-		getNotes:function(clientId){
-			fetch('api/notesOnClients.php?clientId='+clientId)
-				.then(response => response.json())
-				.then(json => {
-					clientApp.notes = json
-				})
-				.catch(err => {
-					console.log('Client fetch error: ');
-					console.log(err);
-				})
-		}
-	},
+		submitNote(e, id) {
+			// TODO: Check validity in a better way
+			this.noteForm.clientId = id;
+			const note = JSON.stringify(this.noteForm);
+			console.log(note);
 
-	created() {
-		fetch('api/client.php')
+			// POST to api
+			fetch('api/notesOnClients.php', {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json; charset=utf-8"
+				},
+				body: note
+			})
 			.then(response => response.json())
 			.then(json => {
-				clientApp.clients = json
+				this.notes.push(json)
 			})
 			.catch(err => {
-				console.log('Client fetch error: ');
-				console.log(err);
-			})
+				console.error('Notes post error:');
+				console.error(err);
+			});
+			// Reset workForm
+			this.noteForm = this.getEmptyNotes(id);
+		}
+	},
+	getEmptyNotes(id) {
+		return {
+			clientId: id,
+			notes: ''
+		}
+	},
+	showModal: function (client) {
+		this.id=client.clientId;
+		this.notes=[];
+		fetch('api/notesOnClients.php?clientId='+clientId)
+		.then(response => response.json())
+		.then(json => {
+			clientApp.notes = json
+		})
+		.catch(err => {
+			console.log('Client fetch error: ');
+			console.log(err);
+		})
+		$("#notesModal").modal('show');
 	}
+},
+
+created() {
+	fetch('api/client.php')
+	.then(response => response.json())
+	.then(json => {
+		clientApp.clients = json
+	})
+	.catch(err => {
+		console.log('Client fetch error: ');
+		console.log(err);
+	})
+}
 });
